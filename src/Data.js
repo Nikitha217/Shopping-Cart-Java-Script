cdeadca1f8c82bd9e253a4a10eb6b707b0e0fedb
Nikitha217 <nikisekarot7@gmail.com>
2025-01-14 18:52:41 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Kids toy",
+    price: 100,
+    desc: "A vibrant, interactive toy that sparks creativity and enhances imaginative play.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Wooden Helicopter",
+    price: 300,
+    desc: "A finely crafted wooden helicopter, offering durability and endless imaginative play for kids.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Wooden Giraffe",
+    price: 250,
+    desc: "A charming wooden giraffe, expertly crafted for sturdy play and sparking creativity in children.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Soft Dolphin Toy",
+    price: 600,
+    desc: "A cuddly soft dolphin toy, perfect for gentle hugs and imaginative play.",
+    img: "images/img-4.jpg",
+  },
+];
